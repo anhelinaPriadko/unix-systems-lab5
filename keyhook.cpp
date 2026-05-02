52a70b5af592c435f240533b4a2ea8011cd5daca
Anhelina Priadko <anhelina.pryadko@gmail.com>
2026-05-02 21:36:58 +0000

    fix wrong file bug

--- keyhook.cpp
+++ keyhook.cpp
@@ -1,42 +1,27 @@
-// process_b.cpp
+// keyhook.cpp
 #include <iostream>
 #include <fcntl.h>
+#include <linux/input.h>
 #include <unistd.h>
-#include <sys/ptrace.h>
-#include <sys/wait.h>
 
 int main() {
-    pid_t target_pid;
-    unsigned long address;
+    const char* dev = "/dev/input/event3"; 
+    int fd = open(dev, O_RDONLY);
 
-    std::cout << "Введіть PID цілі: ";
-    std::cin >> target_pid;
-    std::cout << "Введіть адресу змінної: ";
-    std::cin >> std::hex >> address;
-
-    char mem_path[64];
-    sprintf(mem_path, "/proc/%d/mem", target_pid);
-
-    if (ptrace(PTRACE_ATTACH, target_pid, NULL, NULL) == -1) {
-        perror("ptrace attach");
-        return 1;
-    }
-    waitpid(target_pid, NULL, 0);
-
-    int fd = open(mem_path, O_RDONLY);
     if (fd == -1) {
-        perror("open mem");
+        perror("Не вдалося відкрити пристрій (спробуйте sudo)");
         return 1;
     }
 
-    int buffer;
-    while (true) {
-        pread(fd, &buffer, sizeof(int), address);
-        std::cout << "\rЗчитане значення: " << std::dec << buffer << std::flush;
-        usleep(500000); // 0.5 сек
+    struct input_event ev;
+    std::cout << "Слухаю клавіатуру... (натисніть Ctrl+C для виходу)\n";
+
+    while (read(fd, &ev, sizeof(ev)) > 0) {
+        if (ev.type == EV_KEY && ev.value == 1) {
+            std::cout << "Натиснуто клавішу з кодом: " << ev.code << std::endl;
+        }
     }
 
     close(fd);
-    ptrace(PTRACE_DETACH, target_pid, NULL, NULL);
     return 0;
 }
